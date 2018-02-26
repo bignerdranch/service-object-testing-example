f0ff96c7205856308aa7fdacf9318c05296bf178
Josh Justice <jjustice@bignerdranch.com>
2018-02-26 16:13:53 -0500

    Get test compiling

---
+++ src/main/java/com/bignerdranch/sample/excelgenerator/PrepareSpreadsheet.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.sample.excelgenerator;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class PrepareSpreadsheet {
+    public Workbook call(String departmentId, int year, int month, int day) {
+        return null;
+    }
+}
