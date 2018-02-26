48e7488eff75c27f2c311de300fb5a02a330dc1e
Josh Justice <jjustice@bignerdranch.com>
2018-02-26 16:17:41 -0500

    Move implementation into service object

--- src/main/java/com/bignerdranch/sample/excelgenerator/PrepareSpreadsheet.java
+++ src/main/java/com/bignerdranch/sample/excelgenerator/PrepareSpreadsheet.java
@@ -1,9 +1,25 @@
 package com.bignerdranch.sample.excelgenerator;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 
+import java.io.IOException;
+import java.util.Map;
+
 public class PrepareSpreadsheet {
-    public Workbook call(String departmentId, int year, int month, int day) {
-        return null;
+    Cache cache = new Cache();
+    DataSource dataSource = new HardCodedDataSource();
+    ExcelRenderer excelRenderer = new JxlsRenderer();
+
+    public Workbook call(String departmentId, int year, int month, int day) throws IOException, InvalidFormatException {
+        Workbook workbook = cache.getDepartmentSpreadsheet(departmentId, year, month, day);
+
+        if (workbook == null) {
+            Map departmentData = dataSource.getDepartmentData(departmentId, year, month, day);
+            workbook = excelRenderer.render(departmentData);
+            cache.putDepartmentSpreadsheet(departmentId, year, month, day, workbook);
+        }
+
+        return workbook;
     }
 }
